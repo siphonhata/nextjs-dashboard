82f76fd93757d768d6e391b32372a7b76f15d7ef
siphonhata <siphonhata@gmail.com>
2025-01-16 15:34:46 +0200

    imports updated

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,7 +1,7 @@
+import { LatestInvoice } from '@/app/lib/definitions';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '../fonts';
+
 
 export default async function LatestInvoices({
   latestInvoices,
