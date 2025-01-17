c4747979c518aaaeb1661d8fd7814900f91dd2e8
siphonhata <siphonhata@gmail.com>
2025-01-17 10:39:11 +0200

    added streaming feature

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,7 +18,7 @@ export async function fetchRevenue() {
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
+ 
     // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,6 +1,8 @@
 import { LatestInvoice } from '@/app/lib/definitions';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '../fonts';
+import clsx from 'clsx';
+import Image from 'next/image';
 
 
 export default async function LatestInvoices({
@@ -16,7 +18,7 @@ export default async function LatestInvoices({
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         {/* NOTE: Uncomment this code in Chapter 7 */}
 
-        {/* <div className="bg-white px-6">
+        <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
@@ -53,7 +55,7 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
+        </div>
         <div className="flex items-center pb-2 pt-6">
           <ArrowPathIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
