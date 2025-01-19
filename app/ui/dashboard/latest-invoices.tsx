ede854015d08b14a9ed873a40fca4eabee683119
siphonhata <siphonhata@gmail.com>
2025-01-19 14:04:33 +0200

    Search feature

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,13 +1,13 @@
-import { LatestInvoice } from '@/app/lib/definitions';
+
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '../fonts';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices() { // Remove props
+export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
-  
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
