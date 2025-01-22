78bbb782c6315708ac0e96d94f450d0b729b98ac
siphonhata <siphonhata@gmail.com>
2025-01-22 14:51:08 +0200

    testing

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,12 +5,7 @@ export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default async function Customers(props: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-  }>;
-}) {
+export default async function Customers() {
 
   return (
     <div className='w-full'>
